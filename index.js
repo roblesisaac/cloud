8400017768c2a2946529327ea48c4afb533f8b80
roblesisaac <irobles1030@gmail.com>
2022-04-28 18:49:04 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-  api.get("/api", async (req, res) => {
-    res.send("<h1>Hello Serverless Cloud!</h1>");
-  });
+import { api } from "@serverless/cloud";
+
+api.get("/api", async (req, res) => {
+  res.send("<h1>Hello Serverless Cloud!</h1>");
+});
