1b7aae82b6313ad1e70e70bd053785e0b3cdc689
roblesisaac <irobles1030@gmail.com>
2022-04-28 18:50:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-import { api } from "@serverless/cloud";
-const { Peach, convert, obj, type } = require("natives/peach");
+try {
+  
+  import { api } from "@serverless/cloud";
+  const { Peach, convert, obj, type } = require("natives/peach");
 
-api.get("/api", async (req, res) => {
-  res.send("<h1>Hello Serverless Cloud!</h1>");
-});
+  api.get("/api", async (req, res) => {
+    res.send("<h1>Hello Serverless Cloud!</h1>");
+  });
+  
+} catch (error) {
+  
+  api.get("/api", async (req, res) => {
+    res.send("<h1>Hello ERROR!</h1>");
+  });
+  
+}
