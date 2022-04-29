44a404d557861e1d3f264b9964b60c80ff908eb6
roblesisaac <irobles1030@gmail.com>
2022-04-29 10:28:34 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { api } from "@serverless/cloud";
-import { test } from "./db.js";
-// import { Peach, convert, obj, type } from "./natives/peach.js";
+// import { test } from "./db.js";
+import { Peach, convert, obj, type } from "./natives/peach.js";
 
 api.get("/api", async (req, res) => {
   res.send(`<h1>${test()}</h1>`);
