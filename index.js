2151ecf17ccf33d8f839e1beba965573965b04b3
roblesisaac <irobles1030@gmail.com>
2022-04-29 10:54:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
 import { api } from "@serverless/cloud";
 import { Peach, convert, obj, type } from "./natives/peach.js";
 
-const server = new Peach({
-  steps: {
-    respond: function (req, res) {
-      res.send(`<h1>hello ${ req.params.name } ${req.query.last}!!</h1>`)
-    }
-  },
-  instruct: {
-    send: (req, res) => [
-      { respond: [req, res] }
-    ]
-  }
-  });
-
-api.get("/api/:name", async (req, res) => {
-
-  server.send(req, res);
-  
-});
