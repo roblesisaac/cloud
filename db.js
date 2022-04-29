59e9056ec2aea138982bbe820a5f93772f9538a6
roblesisaac <irobles1030@gmail.com>
2022-04-29 11:35:54 -0700

    Update db.js

--- db.js
+++ db.js
@@ -21,7 +21,7 @@ const db = new Peach({
   }
 });
 
-api.get("/db/:name", async (req, res) => {
+api.get("/db/:name", (req, res) => {
   
   db.respond(req, res, function() {
     return `<h1> Hi ${ this.name } ${this.lastName} </h1>`
