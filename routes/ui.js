604dd76ed43d36f5b14fa2f7eb62e2802d4c9e39
roblesisaac <irobles1030@gmail.com>
2022-04-29 15:25:05 -0700

    Update ui.js

--- routes/ui.js
+++ routes/ui.js
@@ -6,7 +6,7 @@ const ui = new Peach({
     serveUi: function() {
       const { res, sheetName } = this;
       
-      res.send("serving uisheet.com " + sheetName);
+      res.send("serving www.uisheet.com " + sheetName);
     }
   },
   instruct: {
