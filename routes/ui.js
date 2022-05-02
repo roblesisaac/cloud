9fb26ae99f8e53509539076cb76070372e4596fd
roblesisaac <irobles1030@gmail.com>
2022-05-02 12:12:14 -0700

    Update ui.js

--- routes/ui.js
+++ routes/ui.js
@@ -1,4 +1,4 @@
-import { api } from "@serverless/cloud";
+import { api, params } from "@serverless/cloud";
 import { Peach } from "../natives/peach.js";
 
 const ui = new Peach({
@@ -22,7 +22,7 @@ const ui = new Peach({
 //         "route"
        ];
       
-      var data = {};
+      var data = { params: params };
       
       keys.forEach(key => data[key] = req[key]);
       
