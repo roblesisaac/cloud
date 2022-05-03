71665770d1303e6cb14895031d7061a1aa491327
roblesisaac <irobles1030@gmail.com>
2022-05-03 08:14:45 -0700

    Update globalSteps.js

--- natives/globalSteps.js
+++ natives/globalSteps.js
@@ -12,6 +12,39 @@ const globalSteps = {
     var message = obj.deep(this, messageProp);
     alert(message || messageProp);
   },
+  concat: function(dataProp) {
+    var data = obj.deep(this, dataProp),
+        to = obj.tip(this, this.to),
+        { item, prop } = to;
+    
+    item[prop] = item[prop].concat(data); 
+  },
+  download: function (data, next) {
+    data = obj.deep(this, data) || data;
+    
+    if(!data) {
+      console.error("No Data");
+      return;
+    }
+    
+    var filename = this.filename || "download.json";
+    
+    if(typeof data == "object"){
+      data = JSON.stringify(data, undefined, 4);
+    }
+    
+    var blob = new Blob([data], {type: "text/json"}),
+        e    = document.createEvent("MouseEvents"),
+        a    = document.createElement("a");
+        
+    a.download = filename;
+    a.href = window.URL.createObjectURL(blob);
+    a.dataset.downloadurl =  ["text/json", a.download, a.href].join(":");
+    e.initMouseEvent("click", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    a.dispatchEvent(e);
+    
+    next();
+  },
   each: function(res, next) {
     var each = this._step.each,
         getData = each.each,
@@ -77,9 +110,20 @@ const globalSteps = {
       answer.method(this, next);
     });
   },
-  incr: function(prop) {
-    var counter = obj.tip(this, prop);
-    counter.item[counter.prop]++;
+  incr: function(prop, amount) {
+    var counter = obj.tip(this, prop),
+        { item, prop } = counter;
+    
+    if(amount) {
+      item[prop] = item[prop] + amount;
+    } else {
+      item[prop]++; 
+    }
+  },
+  isEmpty: function(arrProp) {
+    var arr = obj.deep(this, arrProp) || arr;
+    
+    this.next(Array.isArray(arr) && !arr.length);
   },
   isString: function(item) {
     var constant = obj.deep(this, item);
@@ -102,6 +146,6 @@ const globalSteps = {
   wait: function(time, next) {
     setTimeout(next, time * 1000);
   }
-}
+};
 
 export { globalSteps };
