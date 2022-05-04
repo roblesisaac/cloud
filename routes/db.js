ffa94536b6293a1d10814407dccf289ebe93f779
roblesisaac <irobles1030@gmail.com>
2022-05-04 14:50:02 -0700

    Update db.js

--- routes/db.js
+++ routes/db.js
@@ -1,5 +1,16 @@
-import { api } from "@serverless/cloud";
+import { api, params } from "@serverless/cloud";
 import { Peach, convert, obj, type } from "../natives/peach.js";
+
+curl --location --request POST 'https://data.mongodb-api.com/app/data-ntloz/endpoint/data/beta/action/findOne' \
+--header 'Content-Type: application/json' \
+--header 'Access-Control-Request-Headers: *' \
+--header `api-key: ${params.MDB}` \
+--data-raw '{
+    "collection":"<COLLECTION_NAME>",
+    "database":"<DATABASE_NAME>",
+    "dataSource":"peach",
+    "projection": {"_id": 1}
+}'
 
 const db = new Peach({
   steps: {
