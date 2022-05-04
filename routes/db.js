6dd005a8ff354ce7c4a0057b7e8779a0b2b6fc07
roblesisaac <irobles1030@gmail.com>
2022-05-04 16:11:30 -0700

    Update db.js

--- routes/db.js
+++ routes/db.js
@@ -1,19 +1,30 @@
 import { api, params } from "@serverless/cloud";
 import { Peach, convert, obj, type } from "../natives/peach.js";
-
-curl --location --request POST 'https://data.mongodb-api.com/app/data-ntloz/endpoint/data/beta/action/findOne' \
---header 'Content-Type: application/json' \
---header 'Access-Control-Request-Headers: *' \
---header `api-key: ${params.MDB}` \
---data-raw '{
-    "collection":"<COLLECTION_NAME>",
-    "database":"<DATABASE_NAME>",
-    "dataSource":"peach",
-    "projection": {"_id": 1}
-}'
+import fetch from "node-fetch";
 
 const db = new Peach({
   steps: {
+    dbApi: function() {
+      const body = {
+        collection:"sheets",
+        database:"uisheet",
+        dataSource:"peach",
+        projection: {"_id": 1}
+      };
+      
+      const response = await fetch(params.MDE+"find", {
+      	method: "post",
+      	body: JSON.stringify(body),
+      	headers: {
+      	  "Content-Type": "application/json",
+      	  "Access-Control-Request-Headers": "*",
+      	  "api-key": params.MDB
+      	}
+      });
+      
+      const data = await response.json();
+      this.next(data);
+    },
     serve: function(message) {
       const { res } = this;
       
@@ -22,12 +33,13 @@ const db = new Peach({
   },
   instruct: {
     respond: (req, res, message) => [
-      { 
-        res,
-        name: req.params.name,
-        lastName: req.query.last
-      },
-      { serve: message }
+      "dbApi"
+      // { 
+      //   res,
+      //   name: req.params.name,
+      //   lastName: req.query.last
+      // },
+      // { serve: message }
     ]
   }
 });
