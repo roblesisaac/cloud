7de99a24ff7b325d5654d1a3e14f2cbe1197ec5c
roblesisaac <irobles1030@gmail.com>
2022-05-04 17:21:17 -0700

    Update db.js

--- routes/db.js
+++ routes/db.js
@@ -4,45 +4,45 @@ import fetch from "node-fetch";
 
 export default api.get("/:name/db", (req, res) => {
   
-const db = new Peach({
-  steps: {
-    dbApi: function(last, next) {
-      const body = {
-        collection:"sheets",
-        database:"uisheet",
-        dataSource:"peach"
-      };
-      
-      const headers = {
-        "Content-Type": "application/json",
-        "Access-Control-Request-Headers": "*",
-        "api-key": params.MDB
-      };
-      
-      const request = {
-      	method: "post",
-      	body: JSON.stringify(body),
-      	headers
-      };
-      
-      const url = params.MDE+"find";
-      
-      fetch(url, request).then(res => res.json())
-        .then(next)
-        .catch(next);
+  const db = new Peach({
+    steps: {
+      dbApi: function(last, next) {
+        const body = {
+          collection: "sheets",
+          database: "uisheet",
+          dataSource: "peach"
+        };
+        
+        const headers = {
+          "Content-Type": "application/json",
+          "Access-Control-Request-Headers": "*",
+          "api-key": params.MDB
+        };
+        
+        const request = {
+        	method: "post",
+        	body: JSON.stringify(body),
+        	headers
+        };
+        
+        const url = params.MDE+"find";
+        
+        fetch(url, request).then(res => res.json())
+          .then(next)
+          .catch(next);
+      },
+      serve: function(data) {      
+        res.send(data);
+      }
     },
-    serve: function(data) {      
-      res.send(data);
+    instruct: {
+      respond: [
+        "dbApi",
+        "serve"
+      ]
     }
-  },
-  instruct: {
-    respond: [
-      "dbApi",
-      "serve"
-    ]
-  }
-});
-  
-db.respond();
+  });
+    
+  db.respond();
   
 });
