ca91e93bdbf931a419bdf51363757486995f3ad8
roblesisaac <irobles1030@gmail.com>
2022-05-04 17:00:38 -0700

    Update db.js

--- routes/db.js
+++ routes/db.js
@@ -1,30 +1,30 @@
 import { api, params } from "@serverless/cloud";
 import { Peach, convert, obj, type } from "../natives/peach.js";
-// import fetch from "node-fetch";
+import fetch from "node-fetch";
 
 const db = new Peach({
   steps: {
-//     dbApi: function() {
-//       const body = {
-//         collection:"sheets",
-//         database:"uisheet",
-//         dataSource:"peach",
-//         projection: {"_id": 1}
-//       };
+    dbApi: function() {
+      const body = {
+        collection:"sheets",
+        database:"uisheet",
+        dataSource:"peach",
+        projection: {"_id": 1}
+      };
       
-//       const resp = await fetch(params.MDE+"find", {
-//       	method: "post",
-//       	body: JSON.stringify(body),
-//       	headers: {
-//       	  "Content-Type": "application/json",
-//       	  "Access-Control-Request-Headers": "*",
-//       	  "api-key": params.MDB
-//       	}
-//       });
+      const resp = await fetch(params.MDE+"find", {
+      	method: "post",
+      	body: JSON.stringify(body),
+      	headers: {
+      	  "Content-Type": "application/json",
+      	  "Access-Control-Request-Headers": "*",
+      	  "api-key": params.MDB
+      	}
+      });
       
-//       const data = await resp.json();
-//       this.next(data);
-//     },
+      const data = await resp.json();
+      this.next(data);
+    },
     serve: function(data) {
       const { rez } = this;
       
