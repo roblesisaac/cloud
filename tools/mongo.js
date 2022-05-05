4801fc5b68ceea0fb0b3f2d3fadc199471f429df
roblesisaac <irobles1030@gmail.com>
2022-05-05 16:54:55 -0700

    Update mongo.js

--- tools/mongo.js
+++ tools/mongo.js
@@ -40,7 +40,7 @@ export default new Peach({
         const formats = {
             limit: Number,
             skip: Number,
-            test: () => "test"
+            select: () => "select"
         };
             
         Object.keys(options).forEach(prop => {
