872694ebdec23b4e040878a23f8cd4729784a189
roblesisaac <irobles1030@gmail.com>
2022-05-04 17:02:07 -0700

    Update db.js

--- routes/db.js
+++ routes/db.js
@@ -5,25 +5,25 @@ import fetch from "node-fetch";
 const db = new Peach({
   steps: {
     dbApi: function() {
-//       const body = {
-//         collection:"sheets",
-//         database:"uisheet",
-//         dataSource:"peach",
-//         projection: {"_id": 1}
-//       };
+      const body = {
+        collection:"sheets",
+        database:"uisheet",
+        dataSource:"peach",
+        projection: {"_id": 1}
+      };
       
-//       const resp = await fetch(params.MDE+"find", {
-//       	method: "post",
-//       	body: JSON.stringify(body),
-//       	headers: {
-//       	  "Content-Type": "application/json",
-//       	  "Access-Control-Request-Headers": "*",
-//       	  "api-key": params.MDB
-//       	}
-//       });
+      const resp = await fetch(params.MDE+"find", {
+      	method: "post",
+      	body: JSON.stringify(body),
+      	headers: {
+      	  "Content-Type": "application/json",
+      	  "Access-Control-Request-Headers": "*",
+      	  "api-key": params.MDB
+      	}
+      });
       
-//       const data = await resp.json();
-      this.next("hi");
+      const data = await resp.json();
+      this.next(data);
     },
     serve: function(data) {
       const { rez } = this;
