ba213d9f5af4eb642deb665f8604d7d07f9abff5
roblesisaac <irobles1030@gmail.com>
2022-05-04 18:42:16 -0700

    Update db.js

--- routes/db.js
+++ routes/db.js
@@ -2,27 +2,26 @@ import { api } from "@serverless/cloud";
 import { Peach } from "../natives/peach.js";
 import db from "../tools/mongo.js";
 
-// const handler = new Peach({
-//     steps: {
-//       fetch: function(last, req, next) {
-//         const collection = req.params.sheetName;
-//         const filter = req.body || req.query;
+const handler = new Peach({
+    steps: {
+      fetch: function(last, req, next) {
+        const collection = req.params.sheetName;
+        const filter = req.body || req.query;
         
-//         db.get(collection, { filter }).then(next);
-//       },
-//       serve: function(last) {
-//         const { res } = this;
-//         res.json("last")
-//       }
-//     },
-//     instruct: (req, res) => [
-//       { res },
+        db.get(collection, { filter }).then(next);
+      },
+      serve: function(last) {
+        const { res } = this;
+        res.json("last")
+      }
+    },
+    instruct: (req, res) => [
+      { res },
 //       { fetch: req },
-//       "serve"
-//     ]
-//   })
+      "serve"
+    ]
+  });
 
 export default api.get("/:sheetName/db", (req, res) => { 
-    res.send("hi");
-//     handler.run(req, res);  
+    handler.run(req, res);  
 });
