f1a46df61ccb4c9c1b939e77c2149a4e6a4a0ff6
roblesisaac <irobles1030@gmail.com>
2022-05-04 17:59:36 -0700

    Update db.js

--- routes/db.js
+++ routes/db.js
@@ -6,7 +6,7 @@ export default api.get("/:sheetName/db", (req, res) => {
   
   new Peach({
     steps: {
-      serve: function(last, next) {
+      fetch: function(last, next) {
         const collection = req.params.sheetName;
         const filter = req.query;
         next(collection);
@@ -15,6 +15,7 @@ export default api.get("/:sheetName/db", (req, res) => {
       serve: (last) => res.json(last)
     },
     instruct: [
+      "fetch",
       "serve"
     ]
   }).run();
