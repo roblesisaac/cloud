cc990e60dd94617a0665fa2a0ce6d2d35e4c8310
roblesisaac <irobles1030@gmail.com>
2022-05-06 13:52:34 -0700

    Update authenticate.js

--- tools/authenticate.js
+++ tools/authenticate.js
@@ -1,7 +1,16 @@
 import { Peach, obj } from "../natives/peach.js";
+import { data } from "@serverless/cloud";
 
 export default new Peach({
+//   steps: {
+//     fetchPermit: function() {
+//       data.getByLabel("label1", username);
+//     }
+//   },
   instruct: {
+//     findPermitByUser: (user) => [
+      
+//     ],
     user: (user) => [
       { log: "user" }
     ]
