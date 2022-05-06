f09092dca0b0225e19ffee4738454abc033fde74
roblesisaac <irobles1030@gmail.com>
2022-05-06 13:41:00 -0700

    Update authenticate.js

--- tools/authenticate.js
+++ tools/authenticate.js
@@ -3,7 +3,7 @@ import { Peach, obj } from "../natives/peach.js";
 export default new Peach({
   instruct: {
     user: (user) => [
-      { end: `<(-_-)> Missing permit, ${user} is...` }
+      { log: "user" }
     ]
   }
 });
