b8242b8495c3445cb5a1ed556ea637d03e4b4d5c
roblesisaac <irobles1030@gmail.com>
2022-05-06 18:44:43 -0700

    Update peach.js

--- natives/peach.js
+++ natives/peach.js
@@ -43,7 +43,9 @@ function buildPeach(instructions, peach, peachName) {
   
   var peachMethod = function(memory, parentSpecial, peachIsForeign) {
     var _args = arguments,
-        userArgs = getArgs(instructions, memory || _args);
+        userArgs = getArgs(instructions, _args);
+        
+    console.log({ memory });
     
     var getMemory = (_resolve, _rej, _peachName) => {
       var isMemory = obj.deep(memory, "constructor.name") == "Memory";
