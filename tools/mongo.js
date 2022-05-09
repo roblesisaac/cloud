1fc83f98e30834fe26f5885b80948941a8a2f3b8
roblesisaac <irobles1030@gmail.com>
2022-05-09 13:07:06 -0700

    Update mongo.js

--- tools/mongo.js
+++ tools/mongo.js
@@ -85,7 +85,6 @@ export default new Peach({
     instruct: {
       handle: (action, collection, options, user) => [
         authenticate.user,
-        { log: action }, { log: collection },
         { concat: action, to: "url" },
         { if: "needsFormat", true: "formatOptions" },
         "fetch"
