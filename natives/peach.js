ec1051f44511499ab420cd97aaf25be9f9a7aa4b
roblesisaac <irobles1030@gmail.com>
2022-05-09 12:55:23 -0700

    Update peach.js

--- natives/peach.js
+++ natives/peach.js
@@ -64,7 +64,7 @@ function buildPeach(instructions, peach, peachName) {
           memory._absorb(peach);
         }
         
-        memory._args = memory._args.unshift(_args);
+        // memory._args = memory._args.unshift(_args);
         
         console.log({ _args: memory._args });
         
